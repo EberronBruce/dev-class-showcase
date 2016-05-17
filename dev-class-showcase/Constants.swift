4b5c6c0935f49279e2259d3ccd730eda7942e10a
Bruce Burgess <bruce@redravencomputing.com>
2016-05-18 02:18:33 +0700

    Added some UI shadows

---
+++ dev-class-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  dev-class-showcase
+//
+//  Created by Bruce Burgess on 5/18/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
