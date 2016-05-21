6d71420c2879e4028b130eb39eb346c8f9554b9b
Bruce Burgess <bruce@redravencomputing.com>
2016-05-22 01:37:55 +0700

    update data from Firebase

--- dev-class-showcase/FeedVC.swift
+++ dev-class-showcase/FeedVC.swift
@@ -7,21 +7,39 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tabelView: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tabelView.delegate = self
-        tabelView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
-            self.tabelView.reloadData()
+            
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+                
+                
+            }
+            
+            self.tableView.reloadData()
         })
 
     }
@@ -31,13 +49,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        let post = posts[indexPath.row]
+        print(post.postDescription)
         
-        return tabelView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 
 
